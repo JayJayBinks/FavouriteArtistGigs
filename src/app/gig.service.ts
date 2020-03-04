17b0cac9a1c9c43633cc4c1a1bd82729e6e5c81d
Julian Wrastil <julian.wrastil@holisticon.de>
2020-03-04 11:36:07 +0100

    adjust basic styling

--- src/app/gig.service.ts
+++ src/app/gig.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Injectable, OnDestroy } from '@angular/core';
+import { Observable, of, Subscription } from 'rxjs';
 import { isArrayLike } from 'rxjs/internal-compatibility';
 import { catchError, tap } from 'rxjs/operators';
 import { isNullOrUndefined } from 'util';
@@ -13,14 +13,20 @@ import { MessageService } from './message.service';
 @Injectable({
   providedIn: 'root'
 })
-export class GigService {
+export class GigService implements OnDestroy {
 
-  constructor(private messageService: MessageService, private http: HttpClient, private errorHandlerService: ErrorHandlerService) { }
-
+  allSubscriptions: Subscription[] = [];
   gigs: GigResource[] = [];
   gigResources: Map<string, GigListResource> = new Map();
   private appKey: string = environment.eventfulApiKey;
   private baseUrl = 'https://cors-anywhere.herokuapp.com/http://api.eventful.com/json';
+
+  constructor(private messageService: MessageService, private http: HttpClient, private errorHandlerService: ErrorHandlerService) { }
+
+  ngOnDestroy(): void {
+    this.allSubscriptions.forEach(s => s.unsubscribe());
+  }
+
   sortByDate = (a: GigResource, b: GigResource) => {
     if (isNullOrUndefined(a.start_time)) {
       return 1;
@@ -30,7 +36,6 @@ export class GigService {
     }
     return new Date(a.start_time).getTime() - new Date(b.start_time).getTime();
   }
-  private searchEventsUrl = artist => this.baseUrl + '/events/search?app_key=' + this.appKey + `&keywords=${artist}&category=music&location=Germany&date=Future`;
 
   getGigs(artist: string): Observable<GigListResource> {
     if (this.gigResources.get(artist) !== undefined) {
@@ -54,15 +59,15 @@ export class GigService {
 
   getGigsForArtists(artists: string[]): GigResource[] {
     const allGigs: GigResource[] = [];
-    artists.forEach(artist => this.getGigs(artist)
-      .subscribe(gigs => {
-        if (isNotNullOrUndefined(gigs.events)) {
-          gigs.events.event
-            .forEach(gig => allGigs.push(gig));
-          allGigs.sort((a, b) => this.sortByDate(a, b));
-        }
-      }));
-    allGigs.sort((a, b) => this.sortByDate(a, b));
+    artists.forEach(artist => this.allSubscriptions.push(
+      this.getGigs(artist)
+        .subscribe(gigs => {
+          if (isNotNullOrUndefined(gigs.events)) {
+            gigs.events.event
+              .forEach(gig => allGigs.push(gig));
+            allGigs.sort((a, b) => this.sortByDate(a, b));
+          }
+        })));
     return allGigs;
   }
 
@@ -94,6 +99,8 @@ export class GigService {
     return performers;
   }
 
+  searchEventsUrl = artist => this.baseUrl + '/events/search?app_key=' + this.appKey + `&keywords=${artist}&category=music&location=Germany&date=Future`;
+
   private containsIgnoreCase(text, other): boolean {
     if (isNullOrUndefined(text) || isNullOrUndefined(other)) {
       return false;
@@ -107,6 +114,9 @@ export class GigService {
 
   private searchTextFields(gig: GigResource, term: string): boolean {
     if (isNotNullOrUndefined(this.getPerformers(gig).find(performer => this.containsIgnoreCase(performer.name, term)))) {
+      return true;
+    }
+    if (this.containsIgnoreCase(gig.comment, term)) {
       return true;
     }
     if (this.containsIgnoreCase(gig.description, term)) {
